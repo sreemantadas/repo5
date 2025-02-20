b5b07ba32213c03223147b916c48832bcd966fcc
sreemantadas <dassreemanta15august@gmail.com>
2025-02-20 10:38:02 +0530

    delete resource group

--- rg.tf
+++ rg.tf
@@ -1,18 +1,18 @@
-resource "azurerm_resource_group" "name" {
-    name = "training-rg"
-    location = "eastus"
-  
-}
-
-resource "azurerm_resource_group" "name1" {
-    name = "training-rg1"
-    location = "eastus2"
-  
-}
-
-resource "azurerm_resource_group" "name4" {
-    name = "training-rg4"
-    location = "westus"
-  
-}
+#resource "azurerm_resource_group" "name" {
+  #  name = "training-rg"
+ #   location = "eastus"
+ # 
+#}
+#
+#resource "azurerm_resource_group" "name1" {
+  #  name = "training-rg1"
+ #   location = "eastus2"
+ # 
+#}
+#
+#resource "azurerm_resource_group" "name4" {
+  #  name = "training-rg4"
+ #   location = "westus"
+ # 
+#}
 
